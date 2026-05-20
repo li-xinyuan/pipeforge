d74acba963f5264a55eddbcbc1b475581f59e3f4
li-xinyuan <lixinyuan@126.com>
2026-05-20 22:58:50 +0800

    fix(pipeforge): add SQLAlchemy dependency and export DatabaseInputPlugin

--- src/pipeforge/plugins/input/__init__.py
+++ src/pipeforge/plugins/input/__init__.py
@@ -1,4 +1,5 @@
 from pipeforge.plugins.input.csv import CsvInputPlugin  # noqa: F401
+from pipeforge.plugins.input.database import DatabaseInputPlugin
 from pipeforge.plugins.input.excel import ExcelInputPlugin
 
-__all__ = ["CsvInputPlugin", "ExcelInputPlugin"]
+__all__ = ["CsvInputPlugin", "DatabaseInputPlugin", "ExcelInputPlugin"]
