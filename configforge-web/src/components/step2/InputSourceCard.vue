b3d983df90e861c74ab6ee46bc45387fc9a4e026
li-xinyuan <lixinyuan@126.com>
2026-05-23 21:43:48 +0800

    fix: move pulse span inside NUpload to preserve v-if/v-else chain

--- configforge-web/src/components/step2/InputSourceCard.vue
+++ configforge-web/src/components/step2/InputSourceCard.vue
@@ -25,18 +25,18 @@
             <NButton text size="tiny" type="error" :disabled="analyzing" @click="removeFile">移除</NButton>
           </div>
         </template>
-        <span :class="{ 'pulse-cta': pulseUpload }" style="display:inline-block;border-radius:8px;">
-          <NUpload
-            v-else
-            :custom-request="handleUpload"
-            :show-file-list="false"
-            :accept="input.plugin === 'csv' ? '.csv' : '.xlsx,.xls'"
-          >
+        <NUpload
+          v-else
+          :custom-request="handleUpload"
+          :show-file-list="false"
+          :accept="input.plugin === 'csv' ? '.csv' : '.xlsx,.xls'"
+        >
+          <span :class="{ 'pulse-cta': pulseUpload }" style="display:inline-block;border-radius:8px;">
             <NButton :loading="uploading" size="small" dashed :disabled="analyzing">
               {{ input.plugin === 'csv' ? '上传 CSV' : '上传 Excel' }}
             </NButton>
-          </NUpload>
-        </span>
+          </span>
+        </NUpload>
         <p v-if="uploadError" class="text-xs text-red-500 mt-1">{{ uploadError }}</p>
       </div>
 
